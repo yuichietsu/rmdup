e59177f89934ad0d7e36fe2539f5aadf57f0af48
yuichietsu <osoba.udon.men@gmail.com>
2024-08-12 22:02:52 +0900

    implemented lzh remover using jlha

--- src/archiver/lzh.rs
+++ src/archiver/lzh.rs
@@ -4,6 +4,11 @@ use std::collections::HashMap;
 use crate::archiver;
 use delharc;
 use crc32fast::Hasher;
+use tempfile::tempdir;
+use std::fs;
+use chrono::prelude::*;
+use std::path::Path;
+use std::process::Command;
 
 pub fn walk(
     file_name : &str,
@@ -60,8 +65,70 @@ pub fn crc(container : &str, path : &str) -> Result<u32, io::Error> {
 }
 
 pub fn remove(container : &str, files : Vec<String>) -> Result<(), Box<dyn Error>> {
-	for file in files {
-		println!("  {} in {}", file, container);
+    let temp_dir = tempdir()?;
+    let mut lha_reader = delharc::parse_file(container)?;
+	let t = temp_dir.path().to_str().unwrap();
+	let mut is_empty = true;
+    loop {
+        let header = lha_reader.header();
+        let filename = header.parse_pathname().display().to_string();
+		let p = format!("{}/{}", t, filename);
+        if files.contains(&filename) {
+			println!("  Removed {}", p);
+		} else {
+			if lha_reader.is_decoder_supported() {
+				let path = Path::new(&p);
+				if let Some(parent) = path.parent() {
+					if !parent.exists() {
+						fs::create_dir_all(parent)?;
+					}
+				}
+				let mut w = fs::File::create(&p)?;
+				io::copy(&mut lha_reader, &mut w)?;
+				is_empty = false;
+			}
+        }
+        if !lha_reader.next_file()? {
+            break;
+        }
+    }
+
+	let now = Local::now();
+	let now_date = now.format("%Y%m%d").to_string();
+	let mut now_time = now.format("%H%M%S").to_string();
+
+	if is_empty {
+        println!("  Removed empty lzh");
+	} else {
+		let mut tmp_file;
+		loop {
+			tmp_file = format!("{}.{}_{}", container, now_date, now_time); 
+			let tmp_path = Path::new(&tmp_file);
+			if !tmp_path.exists() {
+				break;
+			}
+			now_time.push_str("_");
+		}
+
+		let output = Command::new("jlha")
+			.current_dir(t)
+			.arg("a")
+			.arg(&tmp_file)
+			.arg(".")
+			.output()
+			.expect("Failed to execute command");
+
+		if !output.status.success() {
+			eprintln!("Error: {:?}", output);
+		}
+
+        println!(
+            "  {} => {} B",
+            Path::new(container).metadata().unwrap().len(),
+            Path::new(&tmp_file).metadata().unwrap().len()
+         );
 	}
+
+    let _ = archiver::backup_archive(container, &now_date, &now_time);
     Ok(())
 }
