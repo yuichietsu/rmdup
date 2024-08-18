b0963e2a3b18d69451422042c82f8ca39ecb61b0
yuichietsu <osoba.udon.men@gmail.com>
2024-08-19 03:27:04 +0900

    changed rar

--- src/archiver/rar.rs
+++ src/archiver/rar.rs
@@ -14,13 +14,38 @@ pub fn walk(
     map_len   : &mut HashMap<u64, Vec<String>>,
     map_crc   : &mut HashMap<String, u32>,
 ) -> Result<(), Box<dyn Error>> {
-    for entry in Archive::new(file_name).open_for_listing()? {
-        let e = entry.unwrap();
-        if !e.is_directory() {
-            let len  = e.unpacked_size;
-            let name = format!("{}\t{}", file_name, e.filename.display());
-            archiver::push_map_len(map_len, len, name.as_str());
-            map_crc.insert(name, e.file_crc); 
+    let output = Command::new("rar")
+        .arg("lt")
+        .arg(file_name)
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+
+    let mut name = "";
+    let mut is_file = false;
+    let mut size: u64 = 0;
+    let mut crc: u32 = 0;
+    for line in stdout.lines() {
+        let line = line.trim();
+        if line.starts_with("Name:") {
+            name = &line[6..];
+        } else if line.starts_with("Type:") {
+            is_file = &line[6..] == "File"; 
+        } else if line.starts_with("Size:") {
+            size = (&line[6..]).parse()?;
+        } else if line.starts_with("CRC32:") {
+            crc = u32::from_str_radix(&line[7..], 16)?;
+        } else if line == "" && name != "" {
+            if is_file && crc != 0 {
+                let name = format!("{}\t{}", file_name, name);
+                archiver::push_map_len(map_len, size, name.as_str());
+                map_crc.insert(name, crc); 
+            }
+            name = "";
+            is_file = false;
+            size = 0;
+            crc = 0;
         }
     }
     Ok(())
@@ -41,22 +66,37 @@ pub fn remove(container : &str, files : Vec<String>) -> Result<(), Box<dyn Error
 	let t = temp_dir.path().to_str().unwrap();
 	let mut is_empty = true;
 
-    let mut archive = Archive::new(container).open_for_processing().unwrap();
-    while let Some(header) = archive.read_header()? {
-		let filename = header.entry().filename.display().to_string();
-		let p = format!("{}/{}", t, filename);
-        archive = if !files.contains(&filename) {
-			is_empty = false;
-			let path = Path::new(&p);
-			if let Some(parent) = path.parent() {
-				if !parent.exists() {
-					fs::create_dir_all(parent)?;
-				}
-			}
-			header.extract_to(p)?
-        } else {
-			println!("  Removed {}", p);
-            header.skip()?
+    let output = Command::new("rar")
+        .arg("lt")
+        .arg(container)
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+
+    let mut name = "";
+    let mut is_file = false;
+    for line in stdout.lines() {
+        let line = line.trim();
+        if line.starts_with("Name:") {
+            name = &line[6..];
+        } else if line.starts_with("Type:") {
+            is_file = &line[6..] == "File"; 
+        } else if line == "" && name != "" {
+            if is_file && !files.contains(&name.to_string()) {
+                is_empty = false;
+                Command::new("rar")
+                    .current_dir(t)
+                    .arg("x")
+                    .arg(container)
+                    .arg(name)
+                    .output()
+                    .expect("Failed to execute command");
+            } else {
+                println!("  Removed {}", name);
+            }
+            name = "";
+            is_file = false;
         }
     }
 
