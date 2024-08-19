3e89ec336ffa2ff12c918a2a82cc11140182fca4
yuichietsu <osoba.udon.men@gmail.com>
2024-08-19 22:10:52 +0900

    refactoring

--- src/dir.rs
+++ src/dir.rs
@@ -1,6 +1,5 @@
 use std::fs;
 use std::error::Error;
-use std::io;
 use std::collections::HashMap;
 
 use crate::archiver;
@@ -9,7 +8,7 @@ pub fn walk(
     dir     : &str,
     map_len : &mut HashMap<u64, Vec<String>>,
     map_crc : &mut HashMap<String, u32>,
-) -> Result<(), io::Error> {
+) -> Result<(), Box<dyn Error>> {
     let entries = fs::read_dir(dir)?;
     for entry in entries {
         let entry    = entry?;
@@ -22,19 +21,13 @@ pub fn walk(
         } else {
             let lc_path = path.to_lowercase();
             if lc_path.ends_with(".cab") {
-                let _ = archiver::cabinet::walk(path.as_str(), map_len, map_crc);
+                archiver::cabinet::walk(path.as_str(), map_len, map_crc)?;
             } else if lc_path.ends_with(".zip") {
-                let _ = archiver::zip::walk(path.as_str(), map_len, map_crc);
+                archiver::zip::walk(path.as_str(), map_len, map_crc)?;
             } else if lc_path.ends_with(".lzh") {
-                let _ = archiver::lzh::walk(path.as_str(), map_len, map_crc);
+                archiver::lzh::walk(path.as_str(), map_len, map_crc)?;
             } else if lc_path.ends_with(".rar") {
-                match archiver::rar::walk(path.as_str(), map_len, map_crc) {
-                    Ok(()) => {
-                    },
-                    Err(e) => {
-                        eprintln!("SKIP : {} : {}", path, e);
-                    },
-                }
+                archiver::rar::walk(path.as_str(), map_len, map_crc)?;
             } else {
                 let len = metadata.len();
                 if let Some(paths) = map_len.get_mut(&len) {
@@ -68,23 +61,6 @@ pub fn crc(path : &str) -> Result<u32, Box<dyn Error>> {
         }
     }
     Ok(crc)
-}
-
-pub fn remove(path : &str) -> Result<(), Box<dyn Error>>
-{
-    let parts: Vec<&str> = path.split("\t").collect();
-    if parts.len() == 1 {
-    } else {
-        let container = parts[0];
-        let path      = parts[1];
-        let lc_path   = container.to_lowercase();
-        if lc_path.ends_with(".cab") {
-            archiver::cabinet::remove(container, vec![path.to_string()])?;
-        } else if lc_path.ends_with(".zip") {
-            archiver::zip::remove(container, vec![path.to_string()])?;
-        }
-    }
-    Ok(())
 }
 
 pub fn remove_in_archive(container: &str, files: Vec<String>) -> Result<(), Box<dyn Error>>
